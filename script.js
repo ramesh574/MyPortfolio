0dec63bbb334e0aea45b2f1c229ee6f92332530d
ramesh574 <rames15698@gmail.com>
2023-09-21 13:25:51 +0530

    this is my portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+function openProfile(url) {
+  window.open(url, "_blank");
+}
